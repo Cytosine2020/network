a7f88e837188f192e145f8c97a9ab3afe17a5bba
Kaiyuan Xu <xuky@shanghaitech.edu.cn>
2020-11-29 04:44:31 +0800

    basic nat table

--- include/server/nat_server.hpp
+++ include/server/nat_server.hpp
@@ -3,24 +3,63 @@
 
 
 #include <memory>
+#include <atomic>
+#include <unordered_map>
 #include <mutex>
 
 #include "device/mod.hpp"
+#include "wire.hpp"
 
 
 namespace cs120 {
 class NatServer {
 private:
     static const uint16_t NAT_PORTS_BASE = 45678;
+    static const uint16_t NAT_PORTS_SIZE = 1024;
 
     static void *nat_lan_to_wan(void *args_);
 
     static void *nat_wan_to_lan(void *args_);
 
+    static uint32_t get_nat_table_ip(uint64_t value) {
+        return (value & 0x00000000FFFFFFFFLLU) >> 0u;
+    }
+
+    static uint16_t get_nat_table_port(uint64_t value) {
+        return (value & 0x0000FFFF00000000LLU) >> 32u;
+    }
+
+    static uint16_t get_nat_table_extra(uint64_t value) {
+        return (value & 0xFFFF000000000000LLU) >> 48u;
+    }
+
+    static uint64_t assemble_nat_table_field(uint32_t ip, uint16_t port, uint16_t extra) {
+        return static_cast<uint64_t>(ip) |
+               (static_cast<uint64_t>(port) << 32u) |
+               (static_cast<uint64_t>(extra) << 48u);
+    }
+
+    static uint16_t *get_port_from_ip_datagram(MutSlice<uint8_t> datagram) {
+        auto *ip_header = datagram.buffer_cast<struct ip>();
+        auto ip_data = datagram[Range{ip_get_ihl(*ip_header), ip_get_tot_len(*ip_header)}];
+
+        switch (ip_get_protocol(*ip_header)) {
+            case 17:
+            {
+                auto *udp_header = ip_data.buffer_cast<struct udphdr>();
+                if (udp_header == nullptr) { return nullptr; }
+                return &udp_header->uh_dport;
+            }
+            default:
+                return nullptr;
+        }
+    }
+
     pthread_t lan_to_wan, wan_to_lan;
     std::unique_ptr<BaseSocket> lan, wan;
     std::mutex lock;
-    Array<std::pair<uint32_t, uint16_t>> nat_table;
+    Array<std::atomic<uint64_t>> nat_table;
+    std::unordered_map<uint64_t, uint16_t> nat_reverse_table;
 
 public:
     NatServer(std::unique_ptr<BaseSocket> lan, std::unique_ptr<BaseSocket> wan);
--- src/server/nat_server.cpp
+++ src/server/nat_server.cpp
@@ -4,17 +4,41 @@
 namespace cs120 {
 void *NatServer::nat_lan_to_wan(void *args_) {
     auto *args = reinterpret_cast<NatServer *>(args_);
+    size_t lowest_free_port = NAT_PORTS_BASE;
 
     for (;;) {
         auto receive = args->lan->recv();
+        auto *ip_header = receive->buffer_cast<struct ip>();
+        uint32_t dest_ip = ip_get_daddr(*ip_header).s_addr;
 
-        // todo: add new mapping to nat table
+        // todo: check sub net mask
 
         auto send = args->wan->try_send();
         if (send->empty()) {
             cs120_warn("package loss!");
         } else {
+            uint16_t *lan_port_ptr = get_port_from_ip_datagram(*receive);
+
+            auto value = assemble_nat_table_field(dest_ip, ntohs(*lan_port_ptr), 1);
+
+            auto table_ptr = args->nat_reverse_table.find(value);
+            uint16_t wan_port = 0;
+
+            if (table_ptr == args->nat_reverse_table.end()) {
+                wan_port = lowest_free_port;
+                args->nat_table[wan_port].store(value, std::memory_order_seq_cst);
+                args->nat_reverse_table.emplace(value, wan_port);
+            } else {
+                wan_port = table_ptr->second;
+            }
+
             (*send)[Range{0, receive->size()}].copy_from_slice(*receive);
+
+            // todo: modify src ip
+
+            *lan_port_ptr = htonl(wan_port);
+
+            // todo: modify checksum
         }
     }
 
@@ -27,13 +51,26 @@ void *NatServer::nat_wan_to_lan(void *args_) {
     for (;;) {
         auto receive = args->wan->recv();
 
-        // todo: check nat table
+        uint16_t *wan_port_ptr = get_port_from_ip_datagram(*receive);
 
-        auto send = args->lan->try_send();
-        if (send->empty()) {
-            cs120_warn("package loss!");
-        } else {
-            (*send)[Range{0, receive->size()}].copy_from_slice(*receive);
+        auto value = args->nat_table[ntohs(*wan_port_ptr)].load(std::memory_order_seq_cst);
+
+        if (get_nat_table_extra(value) > 0) {
+            auto send = args->lan->try_send();
+            if (send->empty()) {
+                cs120_warn("package loss!");
+            } else {
+                auto lan_ip = get_nat_table_ip(value);
+                auto lan_port = get_nat_table_port(value);
+
+                (*send)[Range{0, receive->size()}].copy_from_slice(*receive);
+
+                // modify headers
+                send->buffer_cast<struct ip>()->ip_dst = in_addr{lan_ip};
+                *wan_port_ptr = htonl(lan_port);
+
+                // todo: modify checksum
+            }
         }
     }
 
@@ -43,8 +80,7 @@ void *NatServer::nat_wan_to_lan(void *args_) {
 NatServer::NatServer(std::unique_ptr<BaseSocket> lan, std::unique_ptr<BaseSocket> wan) :
         lan_to_wan{}, wan_to_lan{},
         lan{std::move(lan)}, wan{std::move(wan)},
-        lock{}, nat_table{1024} {
-
+        lock{}, nat_table{NAT_PORTS_SIZE}, nat_reverse_table{} {
     pthread_create(&lan_to_wan, nullptr, nat_lan_to_wan, this);
     pthread_create(&wan_to_lan, nullptr, nat_wan_to_lan, this);
 }
