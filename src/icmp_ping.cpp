54d815cc490ffb041abf403324f3bc4012976cd9
Kaiyuan Xu <xuky@shanghaitech.edu.cn>
2020-11-29 16:35:41 +0800

    icmp ping usng athernet now

--- src/icmp_ping.cpp
+++ src/icmp_ping.cpp
@@ -1,12 +1,12 @@
 #include "wire.hpp"
-#include "device/raw_socket.hpp"
+#include "device/unix_socket.hpp"
 #include "server/icmp_server.hpp"
 
 
 using namespace cs120;
 
 int main() {
-    std::unique_ptr<BaseSocket> sock(new RawSocket{64});
+    std::unique_ptr<BaseSocket> sock(new UnixSocket{64});
 
-    icmp_ping(std::move(sock), get_local_ip(), inet_addr("182.61.200.7"), 20000);
+    icmp_ping(std::move(sock), inet_addr("192.168.1.2"), inet_addr("182.61.200.7"), 20000);
 }
