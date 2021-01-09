10c40f9e91f29d3276d3a77f5661d715aa6302d1
Zhao <1170635509@qq.com>
2021-01-09 17:10:40 +0800

    ftp command
    add command func

--- src/ftp.cpp
+++ src/ftp.cpp
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <iostream>
 
 #include "utility.hpp"
 #include "wire/wire.hpp"
@@ -17,6 +18,29 @@ uint32_t get_host_ip(const char *host) {
 
 
 int main(int argc, char **argv) {
+//    if (argc != 3) { cs120_abort("accept 2 arguments"); }
+//
+//    std::shared_ptr<BaseSocket> device{new RawSocket{64}};
+//
+//    auto[local_ip, local_port] = parse_ip_address(argv[1]);
+//
+//    EndPoint remote{get_host_ip(argv[2]), 21};
+//    EndPoint local{local_ip, local_port++};
+//
+//    FTPClient ftp_client{device, local, remote};
+//
+//    ftp_client.login("ftp", "");
+//    ftp_client.pwd();
+//    ftp_client.pasv(device, EndPoint{local_ip, local_port++});
+//    ftp_client.list();
+//    ftp_client.cwd("ubuntu");
+//    ftp_client.pwd();
+//    ftp_client.pasv(device, EndPoint{local_ip, local_port++});
+//    ftp_client.list();
+//    ftp_client.pasv(device, EndPoint{local_ip, local_port++});
+//    ftp_client.retr("ls-lR.gz");
+
+
     if (argc != 3) { cs120_abort("accept 2 arguments"); }
 
     std::shared_ptr<BaseSocket> device{new RawSocket{64}};
@@ -27,15 +51,32 @@ int main(int argc, char **argv) {
     EndPoint local{local_ip, local_port++};
 
     FTPClient ftp_client{device, local, remote};
+    for (;;){
+        using namespace std;
+        string str;
+        printf("ftp:");
+        getline(cin,str);
+        size_t pos;
+        if (str.find("PWD")!=string::npos){
+            ftp_client.pwd();
+        }else if ((pos=str.find("CWD"))!=string::npos){
+            ftp_client.cwd(str.substr(pos+4).c_str());
+        }else if (str.find("PASV")!=string::npos){
+            ftp_client.pasv(device,EndPoint{local_ip, local_port++});
+        }else if (str.find("LIST")!=string::npos){
+            if ((pos=str.find(" "))!=string::npos){
+                ftp_client.list(str.substr(pos+1).c_str());
+            }else{
+                ftp_client.list();
+            }
+        } else if ((pos=str.find("RETR"))!=string::npos){
+            ftp_client.retr(str.substr(pos+4).c_str());
+        }else{
+            printf("Not support this command, try again\n");
+        }
 
-    ftp_client.login("ftp", "");
-    ftp_client.pwd();
-    ftp_client.pasv(device, EndPoint{local_ip, local_port++});
-    ftp_client.list();
-    ftp_client.cwd("ubuntu");
-    ftp_client.pwd();
-    ftp_client.pasv(device, EndPoint{local_ip, local_port++});
-    ftp_client.list();
-    ftp_client.pasv(device, EndPoint{local_ip, local_port++});
-    ftp_client.retr("ls-lR.gz");
+    }
+
+
+
 }
